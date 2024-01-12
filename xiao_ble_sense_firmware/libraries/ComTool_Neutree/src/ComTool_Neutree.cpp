48e98984fe83090fa17f4152163fdbc17ff8405a
xuteng lin <xutengl@outlook.com>
2024-01-12 23:11:46 +1100

    feat: comtool uses template to accept int and float

--- xiao_ble_sense_firmware/libraries/ComTool_Neutree/src/ComTool_Neutree.cpp
+++ xiao_ble_sense_firmware/libraries/ComTool_Neutree/src/ComTool_Neutree.cpp
@@ -3,10 +3,7 @@
 
 #include <cassert>
 
-// Default header
-const static uint8_t COMTOOL_HEADDER[] = {0xAA, 0xCC, 0xEE, 0xBB};
-
-static void ComToolPack(uint8_t *buff, int buff_len,
+void ComToolPack(uint8_t *buff, int buff_len,
                                const uint8_t *header, int header_len,
                                const char *name,
                                double x, double y)
@@ -26,14 +23,4 @@ static void ComToolPack(uint8_t *buff, int buff_len,
         sum += buff[i];
     }
     buff[header_len + 1 + len + 8 + 8] = (uint8_t)(sum & 0xff);
-}
-
-// support arduino style plot via Serial
-void ComToolPlot(String name, int value)
-{
-    uint8_t buff[64];
-
-    // use the default header, variable name as name, timestamp as x, value as y
-    ComToolPack(buff, sizeof(buff), COMTOOL_HEADDER, sizeof(COMTOOL_HEADDER), name.c_str(), millis(), value);
-    Serial.write(buff, sizeof(buff));
 }
--- xiao_ble_sense_firmware/libraries/ComTool_Neutree/src/ComTool_Neutree.h
+++ xiao_ble_sense_firmware/libraries/ComTool_Neutree/src/ComTool_Neutree.h
@@ -2,6 +2,20 @@
 
 #include <Arduino.h>
 
+// Default header
+const uint8_t COMTOOL_HEADDER[] = {0xAA, 0xCC, 0xEE, 0xBB};
 
+void ComToolPack(uint8_t *buff, int buff_len,
+                 const uint8_t *header, int header_len,
+                 const char *name,
+                 double x, double y);
 
-void ComToolPlot(String name, int value);
+template <typename T>
+void ComToolPlot(String name, T value)
+{
+    uint8_t buff[64];
+
+    // use the default header, variable name as name, timestamp as x, value as y
+    ComToolPack(buff, sizeof(buff), COMTOOL_HEADDER, sizeof(COMTOOL_HEADDER), name.c_str(), millis(), value);
+    Serial.write(buff, sizeof(buff));
+}
