2ee4042ac697147d00c3a76a5aaf8d7a04f1bf2c
Xuteng Lin <xutengl@outlook.com>
2024-02-14 00:18:13 +1100

    feat: drop detection

--- Tests/Drop_Detection_Test/DropDetection.cpp
+++ Tests/Drop_Detection_Test/DropDetection.cpp
@@ -1,8 +1,55 @@
 #include "dropDetection.h"
 
-void APDS_DataChannel::calib()
+// Function to print a 32-bit unsigned integer in binary format
+static void
+printBinary(uint32_t value)
 {
-    calibValue = raw_u8[0];
+    // Start from the most significant bit (leftmost)
+    for (int i = 31; i >= 0; i--)
+    {
+        // Check if the current bit is set (1) or not (0)
+        printf("%d", (value >> i) & 1);
+
+        // Add a space for better readability
+        if (i % 4 == 0)
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+void plotBinary(uint32_t value)
+{
+    // give each bit a name for better readability, plot each bit
+    const char *names[] = {
+        "U_U", "U_D", "DU_U", "DU_D",
+        "D_U", "D_D", "DD_U", "DD_D",
+        "L_U", "L_D", "DL_U", "DL_D",
+        "R_U", "R_D", "DR_U", "DR_D",
+        "LR_U", "LR_D"};
+    for (int i = 0; i < 18; i++)
+    {
+        Serial.printf("%s:%d,", names[i], (value >> i) & 1);
+    }
+    Serial.printf("\n");
+
+}
+
+void APDS_DataChannel::calib(const bool is_initial = 1)
+{
+    if (is_initial)
+        calibValue = raw_u8[0];
+
+    if (calibValue < raw_u8[0])
+        calibValue++;
+    else if (calibValue > raw_u8[0])
+        calibValue--;
+}
+
+void APDS_DataChannel::copy_buffer()
+{
+    memcpy(raw_u8, buffer, sizeof(uint8_t) * count);
 }
 
 void APDS_DataChannel::zero_offset()
@@ -32,56 +79,150 @@ void APDS_DataChannel::diff()
     {
         dot[i] = lp[i] - lp[i - 1];
     }
+    last_val = lp[count - 1];
 }
 
+void APDS_DataChannel::set_bounds_lp(const float up_b_lp, const float low_b_lp)
+{
+    this->up_b_lp = up_b_lp;
+    this->low_b_lp = low_b_lp;
+}
+void APDS_DataChannel::set_bounds_dot(const float up_b_dot, const float low_b_dot)
+{
+    this->up_b_dot = up_b_dot;
+    this->low_b_dot = low_b_dot;
+}
+
+uint8_t APDS_DataChannel::check_crossing()
+{
+    uint8_t state = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (lp[i] > up_b_lp)
+        {
+            state |= 0x01;
+        }
+        else if (lp[i] < low_b_lp)
+        {
+            state |= 0x2;
+        }
+
+        if (dot[i] > up_b_dot)
+        {
+            state |= 0x4;
+        }
+        else if (dot[i] < low_b_dot)
+        {
+            state |= 0x8;
+        }
+    }
+    return state;
+}
+
+/*
+ * APDS_Data
+ *
+ */
 APDS_Data::APDS_Data()
 {
     sample_count = 0;
 }
 
-void APDS_Data::calib()
+void APDS_Data::set_bounds_lr(const float up_b_lr, const float low_b_lr)
 {
-    u.calib();
-    d.calib();
-    l.calib();
-    r.calib();
+    this->up_b_lr = up_b_lr;
+    this->low_b_lr = low_b_lr;
 }
 
-void APDS_Data::process()
+void APDS_Data::calib(const bool is_initial = 1)
+{
+    u.calib(is_initial);
+    d.calib(is_initial);
+    l.calib(is_initial);
+    r.calib(is_initial);
+}
+
+void APDS_Data::copy_buffer()
 {
     u.count = sample_count;
     d.count = sample_count;
     l.count = sample_count;
     r.count = sample_count;
+    u.copy_buffer();
+    d.copy_buffer();
+    l.copy_buffer();
+    r.copy_buffer();
+}
+
+void APDS_Data::process()
+{
+    copy_buffer();
+
     u.zero_offset();
     d.zero_offset();
     l.zero_offset();
     r.zero_offset();
+
     u.lowpass();
     d.lowpass();
     l.lowpass();
     r.lowpass();
+
     u.diff();
     d.diff();
     l.diff();
     r.diff();
-    
+
+    for (int i = 0; i < sample_count; i++)
+    {
+        lr_diff[i] = l.lp[i] - r.lp[i];
+    }
+}
+
+uint32_t APDS_Data::check_crossing()
+{
+    uint32_t state = 0;
+    // bit field structure
+    // | reserved | lr_state | r_state | l_state | d_state | u_state |
+    // |  32 - 18 |  17 - 16 | 15 - 12 |  11 - 8 |  7 - 4  |  3 - 0  |
+
+    state |= (u.check_crossing() & 0x0F) << 0;
+    state |= (d.check_crossing() & 0x0F) << 4;
+    state |= (l.check_crossing() & 0x0F) << 8;
+    state |= (r.check_crossing() & 0x0F) << 12;
+
+    for (int i = 0; i < sample_count; i++)
+    {
+        if (lr_diff[i] > up_b_lr)
+        {
+            state |= (0x01 << 16);
+        }
+        else if (lr_diff[i] < low_b_lr)
+        {
+            state |= (0x02 << 16);
+        }
+    }
+    return state;
 }
 
 void APDS_Data::printRaw()
 {
     for (int i = 0; i < sample_count; i++)
     {
-        Serial.printf("l=%d, r=%d, u=%d, d=%d\n", l.raw_u8[i], r.raw_u8[i], u.raw_u8[i], d.raw_u8[i]);
+        Serial.printf("ul=%d, ur=%d, uu=%d, ud=%d\n", l.raw_u8[i], r.raw_u8[i], u.raw_u8[i], d.raw_u8[i]);
     }
 }
 
-void APDS_Data::printRaw_i8()
+void APDS_Data::printCalib()
 {
-    Serial.printf("calibValue: cl=%d, cr=%d, cu=%d, cd=%d\n", l.calibValue, r.calibValue, u.calibValue, d.calibValue);
+    Serial.printf("cl=%d, cr=%d, cu=%d, cd=%d\n", l.calibValue, r.calibValue, u.calibValue, d.calibValue);
+}
+
+void APDS_Data::printRaw_i16()
+{
     for (int i = 0; i < sample_count; i++)
     {
-        Serial.printf("l=%d, r=%d, u=%d, d=%d\n", l.raw_i16[i], r.raw_i16[i], u.raw_i16[i], d.raw_i16[i]);
+        Serial.printf("il=%d, ir=%d, iu=%d, id=%d\n", l.raw_i16[i], r.raw_i16[i], u.raw_i16[i], d.raw_i16[i]);
     }
 }
 
@@ -89,7 +230,7 @@ void APDS_Data::printLP()
 {
     for (int i = 0; i < sample_count; i++)
     {
-        Serial.printf("l=%.2f, r=%.2f, u=%.2f, d=%.2f\n", l.lp[i], r.lp[i], u.lp[i], d.lp[i]);
+        Serial.printf("ll=%.2f, lr=%.2f, lu=%.2f, ld=%.2f\n", l.lp[i], r.lp[i], u.lp[i], d.lp[i]);
     }
 }
 
@@ -101,6 +242,18 @@ void APDS_Data::printDot()
     }
 }
 
+void APDS_Data::printLR()
+{
+    for (int i = 0; i < sample_count; i++)
+    {
+        Serial.printf("l-r=%.2f\n", lr_diff[i]);
+    }
+}
+
+/*
+ * APDS-9960 Gesture Sensor
+ *
+ */
 void APDS_DropSensor::init()
 {
     if (!APDS.begin())
@@ -112,18 +265,43 @@ void APDS_DropSensor::init()
         Serial.println("APDS-9960 initialization complete");
     }
 
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 128; i++)
     {
-        data.sample_count = APDS.gestureAvailable(data.u.raw_u8, data.d.raw_u8, data.l.raw_u8, data.r.raw_u8);
+        data.sample_count = APDS.gestureAvailable(data.u.buffer, data.d.buffer, data.l.buffer, data.r.buffer);
+        data.copy_buffer();
         data.printRaw();
+        data.calib(false);
         delay(10);
     }
-    data.calib();
+
+    data.set_bounds_lr(6, -4);
+
+    data.u.set_bounds_lp(4, -4);
+    data.d.set_bounds_lp(4, -4);
+    data.l.set_bounds_lp(4, -4);
+    data.r.set_bounds_lp(4, -4);
+
+    data.u.set_bounds_dot(4, -4);
+    data.d.set_bounds_dot(4, -4);
+    data.l.set_bounds_dot(4, -4);
+    data.r.set_bounds_dot(4, -4);
 }
 
 void APDS_DropSensor::update()
 {
-    data.sample_count = APDS.gestureAvailable(data.u.raw_u8, data.d.raw_u8, data.l.raw_u8, data.r.raw_u8);
+    data.sample_count = APDS.gestureAvailable(data.u.buffer, data.d.buffer, data.l.buffer, data.r.buffer);
     data.process();
-    data.printLP();
+    data.calib(false);
+
+    // data.printRaw();
+    // data.printCalib();
+    // data.printRaw_i16();
+    // data.printLP();
+    // data.printDot();
+    // data.printLR();
+
+    uint32_t value = data.check_crossing();
+    Serial.printf("value=%d\n", value);
+    // printBinary(value);
+    // plotBinary(value);
 }
--- Tests/Drop_Detection_Test/DropDetection.h
+++ Tests/Drop_Detection_Test/DropDetection.h
@@ -6,17 +6,21 @@
 #include <Arduino_APDS9960.h>
 
 #define MAX_SAMPLES 32
-#define MOVING_AVG_SAMPLES 3
+#define MOVING_AVG_SAMPLES 5
+
+
 
 class APDS_DataChannel
 {
 public:
+
     MovingAverage<int16_t, MOVING_AVG_SAMPLES> LP_filter;
     uint8_t count;
     uint8_t calibValue;
-    uint8_t raw_u8[MAX_SAMPLES];
+    uint8_t buffer[MAX_SAMPLES];// buffer for raw data
+    uint8_t raw_u8[MAX_SAMPLES];// data will be copied here to prevent race condition
     int16_t raw_i16[MAX_SAMPLES];
-    float lp[MAX_SAMPLES]; // lowpass 1
+    float lp[MAX_SAMPLES]; // lowpass
     float last_val;
     float dot[MAX_SAMPLES]; // temporal difference
     float up_b_lp, low_b_lp, up_b_dot, low_b_dot;
@@ -24,10 +28,14 @@ public:
 public:
     // constructor
     APDS_DataChannel() {}
-    void calib();
+    void calib(const bool is_initial);
+    void copy_buffer();
     void zero_offset();
     void lowpass();
     void diff();
+    void set_bounds_lp(const float up_b_lp, const float low_b_lp);
+    void set_bounds_dot(const float up_b_dot, const float low_b_dot);
+    uint8_t check_crossing();
 };
 
 class APDS_Data
@@ -44,12 +52,18 @@ public:
 public:
     // constructor
     APDS_Data();
-    void calib();
+    void set_bounds_lr(const float up_b_lr, const float low_b_lr);
+    void calib(const bool is_initial);
+    void copy_buffer();
     void process();
+    uint32_t check_crossing();
+
     void printRaw();
-    void printRaw_i8();
+    void printCalib();
+    void printRaw_i16();
     void printLP();
     void printDot();
+    void printLR();
 };
 
 class APDS_DropSensor
@@ -59,6 +73,7 @@ public:
 
     void init();
     void update();
+
 };
 
 extern APDS_DropSensor drop_sensor;
