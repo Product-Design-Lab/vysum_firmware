389152d122d755a950107e1e2af65d478bfe9cfd
Xuteng Lin <xutengl@outlook.com>
2024-05-15 12:50:35 +1000

    breaking change: put imu in class

--- libraries/PDL_simple_IMU/example/demo/demo.ino
+++ libraries/PDL_simple_IMU/example/demo/demo.ino
@@ -1,16 +1,42 @@
 #include <Arduino.h>
-#include <PDL_simple_IMU.h>
+#include "PDL_Simple_IMU.h"
 
-void setup() 
-{
-    init_imu();
-    Serial.begin(115200);
-    set_debug_status(IMU_DEBUG_STATUS_ANGLE);
-    set_vertical_threshold(10);
-    set_loop_delay(100);
+PDL_Simple_IMU imu;
 
+void onTilted() {
+    Serial.println("Device is tilted");
 }
-void loop() 
-{
+
+void onLevel() {
+    Serial.println("Device is level");
+}
+
+void setup() {
+    // Initialize Serial for debugging outputs
+    Serial.begin(115200);
+    while (!Serial); // Wait for serial port to connect.
+
+    Serial.println("Initializing system...");
+
+    // Initialize the IMU
+    imu.init();
+
+    // Set debug status to display angles
+    imu.setDebugStatus(IMU_DEBUG_STATUS_ANGLE);
+
+    // Set vertical thresholds for X and Y directions
+    imu.setVerticalThresholds(-10, 10, -10, 10);
+
+    // Set loop delay
+    imu.setLoopDelay(100);
+
+    // Set the tilt and level callbacks
+    imu.setTiltedCallback(onTilted);
+    imu.setLevelCallback(onLevel);
+
+    Serial.println("IMU setup complete.");
+}
+
+void loop() {
     delay(1000);
 }
--- libraries/PDL_simple_IMU/src/PDL_simple_IMU.cpp
+++ libraries/PDL_simple_IMU/src/PDL_simple_IMU.cpp
@@ -1,69 +1,61 @@
-#include "PDL_simple_IMU.h"
-
+#include "PDL_Simple_IMU.h"
 #include "Adafruit_TinyUSB.h"
-#include "MovingAverage.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
 #include "math.h"
 
 #define DEFAULT_VERTICAL_THRESHOLD_DEGREES 10
-#define DEFAULT_TASK_PRIORITY 1
+#define DEFAULT_TASK_PRIORITY 2
 #define DEFAULT_LOOP_DELAY_MS 100
 #define TASK_STACK_SIZE 1024
 #define DEFAULT_ANGLE_X_OFFSET 90
 #define DEFAULT_ANGLE_Y_OFFSET 90
 
-LSM6DS3 myIMU(I2C_MODE, 0x6A); // I2C device address 0x6A
-MovingAverage<int16_t, 16> ax_filter;
-MovingAverage<int16_t, 16> ay_filter;
-MovingAverage<int16_t, 16> az_filter;
+PDL_Simple_IMU::PDL_Simple_IMU()
+    : imu(I2C_MODE, 0x6A), angle_x(0), angle_y(0), 
+      x_lower_threshold(-DEFAULT_VERTICAL_THRESHOLD_DEGREES), 
+      x_upper_threshold(DEFAULT_VERTICAL_THRESHOLD_DEGREES), 
+      y_lower_threshold(-DEFAULT_VERTICAL_THRESHOLD_DEGREES), 
+      y_upper_threshold(DEFAULT_VERTICAL_THRESHOLD_DEGREES), 
+      is_vertical(false), was_vertical(false), 
+      loop_delay(DEFAULT_LOOP_DELAY_MS), debug_status(IMU_DEBUG_STATUS_NONE),
+      ax_offset(DEFAULT_ANGLE_X_OFFSET), ay_offset(DEFAULT_ANGLE_Y_OFFSET), imu_task_handle(NULL),
+      tilted_callback(nullptr), level_callback(nullptr) {}
 
-TaskHandle_t imu_task_handle;
+void PDL_Simple_IMU::init(uint32_t priority) {
+    if (imu.begin() != 0) {
+        Serial.println("IMU error");
+    } else {
+        Serial.println("IMU OK!");
+    }
 
-float angle_x = 0;
-float angle_y = 0;
-float vertical_threshold = DEFAULT_VERTICAL_THRESHOLD_DEGREES;
-bool is_vertical = false;
+    xTaskCreate(imuTask, "IMU_TASK", TASK_STACK_SIZE, this, priority, &imu_task_handle);
+}
 
-uint32_t loop_delay = DEFAULT_LOOP_DELAY_MS;
+void PDL_Simple_IMU::imuTask(void *pvParameters) {
+    PDL_Simple_IMU *instance = static_cast<PDL_Simple_IMU*>(pvParameters);
+    instance->xLastWakeTime = xTaskGetTickCount();
+    while (true) {
+        instance->processIMUData();
+        vTaskDelayUntil(&(instance->xLastWakeTime), pdMS_TO_TICKS(instance->loop_delay));
+    }
+}
 
-uint8_t debug_status = 0;
+void PDL_Simple_IMU::processIMUData() {
+    int16_t ax_raw = imu.readRawAccelX();
+    int16_t ay_raw = imu.readRawAccelY();
+    int16_t az_raw = imu.readRawAccelZ();
 
-TickType_t xLastWakeTime;
+    float ax = ax_filter.add(ax_raw);
+    float ay = ay_filter.add(ay_raw);
+    float az = az_filter.add(az_raw);
 
-float ax_offset = DEFAULT_ANGLE_X_OFFSET;
-float ay_offset = DEFAULT_ANGLE_Y_OFFSET;
+    angle_x = atan2(sqrt(ay * ay + az * az), ax) * 180 / M_PI - ax_offset;
+    angle_y = atan2(sqrt(ax * ax + az * az), ay) * 180 / M_PI - ay_offset;
 
-static void imu_task(void *pvParameters)
-{
-    int16_t ax_raw, ay_raw, az_raw;
-    float ax, ay, az;
+    checkTiltStatus();
 
-    while (true)
-    {
-        ax_raw = myIMU.readRawAccelX();
-        ay_raw = myIMU.readRawAccelY();
-        az_raw = myIMU.readRawAccelZ();
-
-        ax = ax_filter.add(ax_raw);
-        ay = ay_filter.add(ay_raw);
-        az = az_filter.add(az_raw);
-
-        angle_x = atan2(sqrt(ay * ay + az * az), ax) * 180 / M_PI - ax_offset;
-        angle_y = atan2(sqrt(ax * ax + az * az), ay) * 180 / M_PI - ay_offset;
-
-        if (abs(angle_x) < vertical_threshold && abs(angle_y) < vertical_threshold)
-        {
-            is_vertical = true;
-        }
-        else
-        {
-            is_vertical = false;
-        }
-
-        switch (debug_status)
-        {
+    switch (debug_status) {
         case IMU_DEBUG_STATUS_RAW:
             Serial.printf("ax:%6d, ay:%6d, az:%6d\n", ax_raw, ay_raw, az_raw);
             break;
@@ -74,62 +66,70 @@ static void imu_task(void *pvParameters)
             Serial.printf("angle_x:%6.3f, angle_y:%6.3f, is_vertical:%d\n", angle_x, angle_y, is_vertical);
             break;
         case IMU_DEBUG_THRESHOLD:
-            Serial.printf("is_vertical:%d\n", is_vertical);
+            Serial.printf("angle_x:%6.3f, angle_y:%6.3f, is_vertical:%d\n", angle_x, angle_y, is_vertical);
             break;
         default:
             break;
-        }
-
-        vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(loop_delay));
     }
 }
 
-void init_imu(uint32_t priority)
-{
-    if (myIMU.begin() != 0)
-    {
-        Serial.println("IMU error");
-    }
-    else
-    {
-        Serial.println("IMU OK!");
-    }
-
-    xTaskCreate(imu_task, "IMU_TASK", TASK_STACK_SIZE, NULL, priority, &imu_task_handle);
-}
-
-void calibrate_imu()
-{
+void PDL_Simple_IMU::calibrate() {
     ax_offset = angle_x;
     ay_offset = angle_y;
 }
 
-void set_debug_status(uint8_t status)
-{
+void PDL_Simple_IMU::setDebugStatus(uint8_t status) {
     debug_status = status;
 }
 
-void set_vertical_threshold(float threshold)
-{
-    vertical_threshold = threshold;
+void PDL_Simple_IMU::setVerticalThresholds(float x_lower, float x_upper, float y_lower, float y_upper) {
+    x_lower_threshold = x_lower;
+    x_upper_threshold = x_upper;
+    y_lower_threshold = y_lower;
+    y_upper_threshold = y_upper;
 }
 
-void set_loop_delay(uint32_t delay_ms)
-{
+void PDL_Simple_IMU::setLoopDelay(uint32_t delay_ms) {
     loop_delay = delay_ms;
 }
 
-float get_angle_x()
-{
+void PDL_Simple_IMU::setSampleSize(uint16_t samples) {
+    ax_filter.set_samples(samples);
+    ay_filter.set_samples(samples);
+    az_filter.set_samples(samples);
+}
+
+void PDL_Simple_IMU::setTiltedCallback(IMUEventCallback callback) {
+    tilted_callback = callback;
+}
+
+void PDL_Simple_IMU::setLevelCallback(IMUEventCallback callback) {
+    level_callback = callback;
+}
+
+float PDL_Simple_IMU::getAngleX() const {
     return angle_x;
 }
 
-float get_angle_y()
-{
+float PDL_Simple_IMU::getAngleY() const {
     return angle_y;
 }
 
-bool isVertical()
-{
+bool PDL_Simple_IMU::isVertical() const {
     return is_vertical;
+}
+
+void PDL_Simple_IMU::checkTiltStatus() {
+    bool currently_vertical = (angle_x > x_lower_threshold && angle_x < x_upper_threshold &&
+                               angle_y > y_lower_threshold && angle_y < y_upper_threshold);
+
+    if (currently_vertical != was_vertical) {
+        was_vertical = currently_vertical;
+        is_vertical = currently_vertical;
+        if (currently_vertical && level_callback) {
+            level_callback();
+        } else if (!currently_vertical && tilted_callback) {
+            tilted_callback();
+        }
+    }
 }
